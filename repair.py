9106d56d85f6efa71c3e0b03892dd773a071e8ad
Leo Andres <lmandres@yahoo.com>
2020-01-31 22:05:21 -0500

    Add moving stand in for character.
    Relates to master

--- repair.py
+++ repair.py
@@ -5,6 +5,8 @@ import pygame
 if __name__ == "__main__":
 
     pygame.init()
+    pygame.key.set_repeat(500, 1)
+
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
     width = screen.get_width()
@@ -13,19 +15,35 @@ if __name__ == "__main__":
     bgimg = pygame.image.load("graphics/sci-fi-level_-_background_layer.png")
 
     done = False
-    width_index = 0
+
+    px = width / 2
+    py = height / 2
 
     while not done:
-
-        width_index -= 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+                elif event.key == pygame.K_UP:
+                    py -= 5
+                elif event.key == pygame.K_RIGHT:
+                    px += 5
+                elif event.key == pygame.K_DOWN:
+                    py += 5
+                elif event.key == pygame.K_LEFT:
+                    px -= 5
 
-        screen.blit(bgimg, (width_index, 0))
+        screen.blit(bgimg, (0, 0))
+        pygame.draw.circle(
+            screen,
+            (255, 255, 255),
+            (int(px), int(py)),
+            10
+        )
+
         pygame.display.flip()
 
     pygame.quit()
