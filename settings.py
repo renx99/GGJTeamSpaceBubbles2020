72bbc2fc714f71d45b3cdb81dc9428b8050bb783
Charlie <thetiguy@gmail.com>
2020-02-01 13:20:23 -0500

    Start cleaning settings

--- settings.py
+++ settings.py
@@ -10,47 +10,70 @@ TITLE = 'Rust Busters <working title>'
 MAP_WIDTH_THRESH = 32
 MAP_HEIGHT_THRESH = 32
 
-TILESIZE = 32 
+TILESIZE = 32
 GRIDWIDTH = WIDTH/TILESIZE
 GRIDHEIGHT = HEIGHT/TILESIZE
 
 # Player Settings
-PLAYER_HEALTH = 100
-PLAY_REGEN = 10
-PLAYER_SPEED = 5
-PLAYER_HIT_RECT = pg.Rect(0,0,35,35)
+PLAYER = {
+        'health': 100,
+        'regen': 10,
+        'speed': 5,
+        'hit_rect': pg.Rect(0, 0, 128, 128),
+        'img': 'temp-player.png'
+    }
+
 WEAPON_OFFSET = vec(30,10)
 
 # Weapon Settings
-WEAPONS = {}
-WEAPONS['wrench'] = {'damage': 50}
+WEAPONS = {
+        'WRENCH': {
+                'range': 2,
+                'damage': 50
+            },
+        'BITE': {
+                'damage': 50,
+                'range': 1,
+            },
+        'GUN': {
+                'range': 5,
+                'damage': 100,
+            }
+    }
 
 # Enemy Settings
-ENEMIES = {}
-ENEMIES['dog'] = {
-                    'damage':20,
-                    'range':1,
-                    'speed':3
-                    }
-ENEMIES['guard'] = {
-                    'damage':34,
-                    'range':5,
-                    'speed':1
-                    }
+ENEMIES = {
+        'DOG': {
+            'speed': 3,
+            'weapon': 'bite',
+            'img': 'temp-dog.png',
+            'hit_rect': pg.Rect(0, 0, 64, 64),
+            'health': 100
+        },
+        'GUARD': {
+            'speed': 1,
+            'weapon': 'gun',
+            'img': 'temp-guard.png',
+            'hit_rect': pg.Rect(0, 0, 128, 128),
+            'health': 200
+        }
+    }
 
 # Layers
-WALL_LAYER = 1
-PLAYER_LAYER = 2
-ENEMY_LAYER = 2
+LAYERS = {
+        'WALL': 1,
+        'PLAYER': 2,
+        'ENEMY': 2,
+    }
 
 # Sounds
-BG_MUSIC = ''
-COMBAT_MUSIC = ''
-PLAYER_HIT_SOUNDS = {}
-ENEMY_HIT_SOUNDS = {}
-ENEMY_HIT_SOUNDS['dog'] = {}
-ENEMY_HIT_SOUNDS['guard'] = {}
-ENEMY_ALERT_SOUNDS = {}
+BG_MUSIC = 'chill.wav'
+COMBAT_MUSIC = 'tense.wav'
+PLAYER_HIT_SOUNDS = {'oof.wav'}
+ENEMY_HIT_SOUNDS = {'blarg.wav'}
+ENEMY_HIT_SOUNDS['dog'] = {'bork.wav'}
+ENEMY_HIT_SOUNDS['guard'] = {'halt.wav'}
+ENEMY_ALERT_SOUNDS = {'alert.wav'}
 ENEMY_ALERT_SOUNDS['dog'] = {}
 ENEMY_ALERT_SOUNDS['guard'] = {}
 WEAPON_SOUNDS = {}
