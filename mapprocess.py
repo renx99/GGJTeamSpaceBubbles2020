3ef83066e60fa661cc8a0e62cc6a9608b6b83a8c
profcommie <profcommie@gmail.com>
2020-02-01 13:05:10 -0500

    Add map class and Camera to mapprocess.py
    Replacing tilemap.py with mapprocess.py

--- mapprocess.py
+++ mapprocess.py
@@ -1,54 +1,83 @@
 import pygame
+from settings import *
 
-TILE_WIDTH = 32
-TILE_HEIGHT = 32
 
-tileimgs = {
-    '0': {
-        "movable": True,
-        "tileimg": pygame.image.load("graphics/tiles/0.png").subsurface((0, TILE_HEIGHT*1, TILE_WIDTH, TILE_HEIGHT))
-    },
-    'A': {
-        "movable": False,
-        "tileimg": pygame.image.load("graphics/tiles/ua.png").subsurface((0, TILE_HEIGHT*1, TILE_WIDTH, TILE_HEIGHT))
-    }
-}
+def collide_hit_rect(one, two):
+    return one.hit_rect.colliderect(two.rect)
 
-def loadmap(fileName):
+class Map:
+    def __init__(self, filename):
+        TILE_WIDTH, TILE_HEIGHT = TILE_SIZE
+        tileimgs = {
+            '0': {
+                "movable": True,
+                "tileimg": pygame.image.load("graphics/tiles/0.png").subsurface((TILE_WIDTH*0, TILE_HEIGHT*1, TILE_WIDTH, TILE_HEIGHT))
+            },
+            'A': {
+                "movable": False,
+                "tileimg": pygame.image.load("graphics/tiles/ua.png").subsurface((TILE_WIDTH*0, TILE_HEIGHT*1, TILE_WIDTH, TILE_HEIGHT))
+            }
+        }
 
-    returnList = []
+    def loadmap(fileName):
 
-    fileIn = open(fileName, "r")
-    row = -1
-    for lineIn in fileIn:
-        row += 1
-        returnList.append([])
-        line = lineIn.strip()
-        tile = None
-        for tileIndex in range(0, len(line), 1):
-            tile = line[tileIndex]
-            returnList[row].append(tile)
-    fileIn.close()
+        returnList = []
 
-    return returnList
+        fileIn = open(fileName, "r")
+        row = -1
+        for lineIn in fileIn:
+            row += 1
+            returnList.append([])
+            line = lineIn.strip()
+            tile = None
+            for tileIndex in range(0, len(line), 1):
+                tile = line[tileIndex]
+                returnList[row].append(tile)
+        fileIn.close()
 
-def gettilemap(maplist):
+        return returnList
 
-    maxRowIndex = len(maplist)
-    maxColIndex = 0
+    def gettilemap(maplist):
 
-    for rowIndex in range(0, len(maplist), 1):
-        if len(maplist[rowIndex]) > maxColIndex:
-            maxColIndex = len(maplist[rowIndex])
+        maxRowIndex = len(maplist)
+        maxColIndex = 0
 
-    returnSurface = pygame.Surface((maxColIndex*TILE_WIDTH, maxRowIndex*TILE_HEIGHT))
+        for rowIndex in range(0, len(maplist), 1):
+            if len(maplist[rowIndex]) > maxColIndex:
+                maxColIndex = len(maplist[rowIndex])
 
-    for rowIndex in range(0, len(maplist), 1):
-        for colIndex in range(0, len(maplist[rowIndex]), 1):
-            tile = maplist[rowIndex][colIndex]
-            returnSurface.blit(
-                tileimgs[tile]["tileimg"],
-                (TILE_WIDTH*colIndex, TILE_HEIGHT*rowIndex)
-            )
-    
-    return returnSurface
+        returnSurface = pygame.Surface((maxColIndex*TILE_WIDTH, maxRowIndex*TILE_HEIGHT))
+
+        for rowIndex in range(0, len(maplist), 1):
+            for colIndex in range(0, len(maplist[rowIndex]), 1):
+                tile = maplist[rowIndex][colIndex]
+                returnSurface.blit(
+                    tileimgs[tile]["tileimg"],
+                    (TILE_WIDTH*colIndex, TILE_HEIGHT*rowIndex)
+                )
+        
+        return returnSurface
+
+
+class Camera:
+    def __init__(self, width, height):
+        self.camera = pg.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def apply(self, entity):
+        return entity.rect.move(self.camera.topleft)
+
+    def apply_rect(self, rect):
+        return rect.move(self.camera.topleft)
+
+    def update(self, target):
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
+
+        # limit scrolling to map size
+        x = min(0, x)  # left
+        y = min(0, y)  # top
+        x = max(-(self.width - WIDTH), x)  # right
+        y = max(-(self.height - HEIGHT), y)  # bottom
+        self.camera = pg.Rect(x, y, self.width, self.height)
