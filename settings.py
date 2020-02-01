c85d8e94d3866ce38e4283068ba803b257f0cc2e
profcommie <profcommie@gmail.com>
2020-02-01 00:42:05 -0500

    Add values to settings Add values to settings for defaults.

--- settings.py
+++ settings.py
@@ -12,6 +12,7 @@ GRIDHEIGHT = HEIGHT/TILESIZE
 
 # Player Settings
 PLAYER_HEALTH = 100
+PLAY_REGEN = 10
 PLAYER_SPEED = 5
 PLAYER_HIT_RECT = pg.Rect(0,0,35,35)
 WEAPON_OFFSET = vec(30,10)
@@ -20,3 +21,26 @@ WEAPON_OFFSET = vec(30,10)
 WEAPONS = {}
 WEAPONS['wrench'] = {'damage': 50}
 
+# Enemy Settings
+ENEMIES = {}
+ENEMIES['dog'] = {
+                    'damage':20,
+                    'range':1,
+                    'speed':3
+                    }
+ENEMIES['guard'] = {
+                    'damage':34,
+                    'range':5,
+                    'speed':1
+                    }
+
+# Layers
+WALL_LAYER = 1
+PLAYER_LAYER = 2
+ENEMY_LAYER = 2
+
+# Sounds
+BG_MUSIC = ''
+COMBAT_MUSIC = ''
+WEAPON_SOUNDS = {}
+EFFECT_SOUNDS = {}
