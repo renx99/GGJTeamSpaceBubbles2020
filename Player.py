0a422435ef0af43436e4b984e82e6a8340b2495b
Rene Lopez <renx99@gmail.com>
2021-01-17 23:28:07 -0500

    initial player/sprite class.

--- Player.py
+++ Player.py
@@ -1,13 +1,29 @@
 import pyglet
 
+
 class Player:
-    def __init__(self, sprite):
+    def __init__(self, sprites):
+        self.anim = {}
+        self.sheet = {}
+        self.grid = {}
+        self.anim = {}
+        self.sprite = {}
 
-        self.sprite = pyglet.resource.image(sprite)
-        self.grid = pyglet.image.ImageGrid(self.sprite, rows=1, columns=4)
-        self.anim = pyglet.image.Animation.from_image_sequence(self.grid, duration=0.25)
+        self.active_anim = "up"
 
-        self.anim = pyglet.sprite.Sprite(self.anim)
+        for action, sprite in sprites.items():
+            sheet = pyglet.resource.image(sprite)
+            grid = pyglet.image.ImageGrid(sheet, rows=1, columns=4)
+            anim = pyglet.image.Animation.from_image_sequence(grid, duration=0.25)
+            self.sprite[action] = pyglet.sprite.Sprite(anim)
+
+            print("action: {}, sprite: {}".format(action, sprite))
+
+        # self.sprite = pyglet.resource.image(sprite)
+        # self.grid = pyglet.image.ImageGrid(self.sprite, rows=1, columns=4)
+        # self.anim = pyglet.image.Animation.from_image_sequence(self.grid, duration=0.25)
+        #
+        # self.anim = pyglet.sprite.Sprite(self.anim)
 
     def draw(self):
-        return self.anim.draw()
+        return self.sprite["right"]
